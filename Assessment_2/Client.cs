91a01f95bb86f9921a8244a3ca08edc80ba49b64
Alp-Gaming <amcd13@outlook.com>
2022-10-11 22:18:49 +1000

    Add project files.

---
+++ Assessment_2/Client.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Assessment_2
+{
+    internal class Client
+    {
+        public string name;
+        public string password;
+        public string email;
+        // put home address variables in here too
+
+        private void check(int check_type) // check_type used to see whether it should check for email syntax of=r password syntax (name needs no syntax)
+        {
+            string email_params = "[A-Za-z0-9_.-]*[A-Za-z0-9]@[A-Za-z0-9-]+[A-Za-z0-9-.]*.[A-Za-z]+";
+            string password_params = "([A-Z]{1,}|[a-z]{1,}|[0-9]{1,}|[!@#$%^&*]{1,}){8,}";
+            switch (check_type)
+            {
+                case 1:
+                    if (!System.Text.RegularExpressions.Regex.IsMatch(email ,email_params))
+                    {
+                        email = null;
+                        Console.WriteLine("     The suplied value is not a valid email");
+                    }
+                    break;
+
+                case 2:
+                    if (!System.Text.RegularExpressions.Regex.IsMatch(password, password_params))
+                    {
+                        password = null;
+                        Console.WriteLine("     The suplied value is not a valid password");
+                    }
+                    break;
+                default:
+                    if (name == "")
+                    {
+                        name = null;
+                        Console.WriteLine("     Please enter a value");
+                    }
+                    break;
+            }
+        }
+
+        public void Register()
+        {
+            Console.WriteLine("Registration\n------------"); // write the stuff here that needs to go be4 the registration happens
+            while(name == null) {
+                Console.WriteLine("\nPlease enter your name");
+                name = Console.ReadLine();
+                if (name == "")
+                {
+                    name = null;
+                }
+            }
+            while(email == null)
+            {
+                Console.WriteLine("\nPlease enter email address");
+                email = Console.ReadLine();
+                check(1);
+            }
+            while(password == null)
+            {
+                Console.WriteLine("\nPlease choose a password" +
+                    "\n* At least 8 characters\n* No white space characters" +
+                    "\n* At least one upper-case letter" +
+                    "\n* At least one lowe case letter" +
+                    "\n* at least one digit" +
+                    "\n* at least  one special character");
+                
+                password = Console.ReadLine();
+                check(2);
+            }
+        }
+        public void Advertise_Product()
+        {
+            Console.WriteLine("Product Advertisment\n------------");
+            string prod_name;
+            string Prod_Init_Price;
+            string Prod_Description;
+            while (true)
+            {
+                Console.WriteLine("\nProduct Name");
+                prod_name = Console.ReadLine();
+                if (Regex.IsMatch(prod_name, "[\x21-\x7E]"))
+                {
+                    break;
+                }
+                Console.WriteLine("\nInitial Price");
+                Prod_Init_Price = Console.ReadLine();
+                if (Regex.IsMatch(Prod_Init_Price, "[0-9]+"))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assessment_2/Program.cs
@@ -0,0 +1,218 @@
+﻿using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Assessment_2
+{
+    internal class Program
+    {
+        static Client logged_in_Client = new Client();
+        public static void Save_Details(string saved_names, string saved_passwords, string saved_emails)
+        {
+            if (File.Exists("saved_Clients.txt"))
+            {
+                using (StreamWriter sw = File.AppendText("saved_Clients.txt"))
+                {
+                    sw.WriteLine("name: " + saved_names + "\nemail: " + saved_emails + "\npassword: " + saved_passwords);
+                }
+            }
+            else
+            {
+                File.WriteAllText("saved_Clients.txt", "name: " + saved_names + "\nemail: " + saved_emails + "\npassword: " + saved_passwords + "\n");
+            }
+        }
+        private static void login()
+        {
+            string email;
+            string password;
+            string details = File.ReadAllText("saved_Clients.txt");
+            Console.WriteLine("\nSign In \n-------\n");
+            string identifier;
+            while (true)
+            {
+                Console.WriteLine("Please Enter Your Email Address");
+                email = Console.ReadLine();
+                string email_regex = "email: " + email + "\n";
+                Match found_email = System.Text.RegularExpressions.Regex.Match(details, email_regex);
+                if (found_email.Success)
+                {
+                    identifier = email;
+                    break;
+                }
+                Console.WriteLine("\nThere was no " + email_regex);
+            }
+            logged_in_Client.email = email;
+            while (true)
+            {
+                Console.WriteLine("\nPlease Enter Your Password");
+                password = Console.ReadLine();
+                string password_regex = identifier + "\npassword: " + password + "\n";
+                Match found_password = System.Text.RegularExpressions.Regex.Match(details, password_regex);
+                if (found_password.Success)
+                {
+                    break;
+                }
+                Console.WriteLine("\nThere was no " + password_regex);
+            }
+            logged_in_Client.password = password;
+
+            Match Clientnum = System.Text.RegularExpressions.Regex.Match(details, "[0-9]+.+\n.+\nemail: " + identifier);
+            Clientnum = Regex.Match(Clientnum.Value, "[0-9]+");
+
+            // do the address input loop here
+            Match does_address_exist = System.Text.RegularExpressions.Regex.Match(details, Clientnum.Value + " has registered address: ");
+            if (!does_address_exist.Success)
+            {
+                int unit_num;
+                int street_num;
+                string street_name;
+                string street_suffix;
+                string city;
+                string state;
+                int postcode;
+                Console.WriteLine("\nPlease Provide Your Home Address");
+                while (true)
+                {
+                    Console.WriteLine("\nUnit number (0 = none):");
+                    string inputted_unit_num = Console.ReadLine();
+                    bool success = Int32.TryParse(inputted_unit_num, out unit_num);
+                    if (success && unit_num >= 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Unit number must be a non-negative integer");
+                }
+                while (true)
+                {
+                    Console.WriteLine("\nStreet number:");
+                    string inputted_street_num = Console.ReadLine();
+                    bool success = Int32.TryParse(inputted_street_num, out street_num);
+                    if (!success)
+                    {
+                        Console.WriteLine("Street number must be a positive integer");
+                    } else if (street_num <= 0)
+                    {
+                        Console.WriteLine("Street number must be greater than zero");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("\nStreet name:");
+                street_name = Console.ReadLine();
+                Console.WriteLine("\nStreet suffix:");
+                street_suffix = Console.ReadLine();
+                Console.WriteLine("\nCity:");
+                city = Console.ReadLine();
+                while (true)
+                {
+                    Console.WriteLine("\nState (ACT, NSW, NT, SA, QLD, TAS, VIC, WA):");
+                    state = Console.ReadLine();
+                    if (state == "ACT"|| state == "NSW"|| state == "NT" || state == "SA"|| state == "QLD"||state == "TAS"||state == "VIC"|| state == "WA"||
+                        state == "act" || state == "nsw" || state == "nt" || state == "sa" || state == "qld" || state == "tas" || state == "vic" || state == "wa")
+                    {
+                        break;
+                    }
+                    Console.WriteLine("state must be either: ACT, NSW, NT, SA, QLD, TAS, VIC or WA");
+                }
+                while (true)
+                {
+                    Console.WriteLine("\nPostcode (1000 .. 9999)");
+                    string input_postcode = Console.ReadLine();
+                    bool success = Int32.TryParse(input_postcode, out postcode);
+                    if (success && postcode > 100 && postcode < 9999)
+                    {
+                        break;
+                    } 
+                }
+                string final_address = unit_num.ToString() + " " + street_num.ToString() + " " + street_name + " " + street_suffix + " " + city + " " + state + " " + postcode;
+                using (StreamWriter sw = File.AppendText("saved_Clients.txt"))
+                {
+                    sw.WriteLine(Clientnum.Value + " has registered address: " + final_address);
+                }
+                if(unit_num == 0)
+                    Console.WriteLine("Address have been updated to " + street_num.ToString() + " " + street_name + " " + street_suffix + ". " + city + " " + state + " " + postcode);
+                else
+                {
+                    Console.WriteLine("Address have been updated to " + unit_num.ToString() + " " + street_num.ToString() + " " + street_name + " " + street_suffix + ". " + city + " " + state + " " + postcode);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int state = 1;
+            bool running = true;
+            while (running)
+            {
+                switch (state)
+                {
+                    case 1:
+                        Console.WriteLine("+--------------------------------+");
+                        Console.WriteLine("|  Welcome To the Auction House  |");
+                        Console.WriteLine("+--------------------------------+");
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Main Menu \n--------- \n(1) Register \n(2) Sign In \n(3) Exit");
+                        Console.WriteLine("\nPlease Select a Number Between 1 and 3");
+                        string input = Console.ReadLine();
+                        switch (input)
+                        {
+                            case "1": // initial sign/register select in page
+                                //TODO(for future me) you only need 2 call the class locally and then you can just store the variables in a file(so u dont need 2 keep multiple objects for each individual user)
+                                Console.WriteLine("");
+                                Client new_user = new Client();
+                                new_user.Register();
+                                Save_Details(new_user.name, new_user.password, new_user.email);
+                                input = null;
+                                state = 1;
+                                break;
+                            case "2":
+                                login();
+                                
+                                state = 2;
+                                input = null;
+                                break;
+                            case "3":
+                                running = false;
+                                input = null;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 2: // Client Menu page
+                        Console.WriteLine("\nClient Menu------\n(1) Advertise Product\n(2) View My Poroduct list\n (3) Search For Advertised Products\n(4) View Bids On My Products\n(5) View My Purchased Itedms\n(6) Log off\n\nPlease select an option between 1 and 6");
+                        input = Console.ReadLine();
+                        switch (input)
+                        {
+                            case "1":
+                                input = null;
+                                logged_in_Client.Advertise_Product();
+                                break;
+                            case "2":
+                                input = null;
+                                break;
+                            case "3":
+                                input = null;
+                                break;
+                            case "4":
+                                input = null;
+                                break;
+                            case "5":
+                                input = null;
+                                break;
+                            case "6":
+                                input = null;
+                                break;
+                            case "7":
+                                input = null;
+                                break;
+                            case "8":
+                                input = null;
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
